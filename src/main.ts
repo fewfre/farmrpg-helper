58b34f746a37b2a0798df0df2c1ed992d714c980
fewfre <wesitup@gmail.com>
2023-07-04 18:02:10 -0400

    tweak to load css in greasyfork

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { Navigation } from './pages/navigation';
 import { PostOfficePasswords } from './pages/postoffice';
 import { Vault } from './pages/vault';
 
+// @ts-ignore ts(2304)
+if (typeof GM_getResourceURL !== 'undefined') {
+	// @ts-ignore ts(2304)
+	GM_addStyle(GM_getResourceURL("scriptStyle"));
+}
+
 Navigation.setup();
 Fishing.setup();
 Exploration.setup();
